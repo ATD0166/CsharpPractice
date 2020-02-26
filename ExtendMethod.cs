8e676ebb59a296f09b2a0d7d2782f8ea798011fc
Asato <asato_chen@outlook.com>
2020-02-26 20:50:40 +0800

    first commit

---
+++ ExtendMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPracticeApp1
+{
+    static public class ExtendMethod
+    {
+        static public char PickInitial(this string s)
+        {
+            char[] chars = s.ToCharArray();
+            return chars[0];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpPracticeApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LINQ TEST
+            int[] numbers = new int[] { 1,2,3,5,15,22,51,98};
+
+            int max = numbers.Max();
+
+            int maxofeven = numbers.Where(num => num % 2 == 0).Max();
+
+            // Lambda寫法
+            IEnumerable<int> evens = numbers.Where(num => num % 2 == 0);
+
+            // 類SQL寫法
+            IEnumerable<int> odds = from nums in numbers 
+                                    where  nums % 2 != 0 
+                                    select nums;
+            
+            foreach (var item in odds)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            // EXTEND METHOD TEST
+            string text = "Text";
+            Console.WriteLine(text.PickInitial());            
+        }
+    }
+}
