0332d8a37faf7f0e3853c5cab8dbb73e4189b143
Asato <asato_chen@outlook.com>
2020-02-26 22:55:41 +0800

    add new practices

--- Program.cs
+++ Program.cs
@@ -2,37 +2,96 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace CSharpPracticeApp1
+namespace LinqPractice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // LINQ TEST
-            int[] numbers = new int[] { 1,2,3,5,15,22,51,98};
-
-            int max = numbers.Max();
-
-            int maxofeven = numbers.Where(num => num % 2 == 0).Max();
-
-            // Lambda寫法
-            IEnumerable<int> evens = numbers.Where(num => num % 2 == 0);
-
-            // 類SQL寫法
-            IEnumerable<int> odds = from nums in numbers 
-                                    where  nums % 2 != 0 
-                                    select nums;
-            
-            foreach (var item in odds)
-            {
-                Console.WriteLine(item);
-            }
-
-
-
             // EXTEND METHOD TEST
             string text = "Text";
-            Console.WriteLine(text.PickInitial());            
+            Console.WriteLine(text.PickInitial());
+            Console.WriteLine();
+
+
+            List<Student> students = Student.GetAllStudents();
+
+
+            // LINQ AGGRECATE OPERATORS
+            //MAX
+            double maxPoint = students.Max(stu => stu.Point);                            
+            Console.WriteLine($"本班最高分 : {maxPoint}");
+            Console.WriteLine();
+
+            //MIN
+            double minPoint = students.Min(stu => stu.Point);
+            Console.WriteLine($"本班最低分 : {minPoint}");
+            Console.WriteLine();
+
+            //SUM
+            double sum = students.Sum(stu => stu.Point);
+            Console.WriteLine($"全班總分 :  {sum}");
+            Console.WriteLine();
+
+            //AVERAGE
+            double average = students.Average(stu => stu.Point);
+            Console.WriteLine($"本班平均分數 : {average}");
+            Console.WriteLine();
+
+            //COUNT
+            int numofPass = students.Where(stu => stu.Point > 59).Count();
+            Console.WriteLine($"本班及格人數 : {numofPass}");
+            Console.WriteLine();
+
+
+            //LINQ PROJECTION OPERATORS
+            //SELECT
+            var ids = students.Where(stu => stu.Point > 59).Select(stu => stu);
+            Console.WriteLine("及格者 : ");
+            foreach (var item in ids)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine();
+
+            //SELECTMANY
+            var stuAbility =  students.SelectMany(stu => stu.Ability,  (student, ability) => new { Name = student.Name, Ability = ability});
+            Console.WriteLine("學生技能");
+            foreach (var item in stuAbility)
+            {
+                Console.WriteLine($"{item.Name} : {item.Ability}");
+            }
+            Console.WriteLine();
+
+            //DISTINCT
+            var allAbility = students.SelectMany(stu => stu.Ability).Distinct();
+            Console.WriteLine("學生技能列表");
+            foreach (var item in allAbility)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //ORDERING OPERATORS
+            //ORDERBY
+            Console.WriteLine("成績由低到高");
+            var sortByAsc = students.OrderBy(stu => stu.Point);
+            foreach (var item in sortByAsc)
+            {
+                Console.WriteLine($"{item.Name} : {item.Point}");
+            }
+            Console.WriteLine();
+
+            //ORDERBYDESCENDING
+            Console.WriteLine("成績由高到低");
+            var sortByDes = students.OrderByDescending(stu => stu.Point);
+            foreach (var item in sortByDes)
+            {
+                Console.WriteLine($"{item.Name} : {item.Point}");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqPractice
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int ID  { get; set; }
+        public double Point { get; set; }
+        public string[] Ability { get; set; }
+
+        public static List<Student> GetAllStudents()
+        {
+            List<Student> stus = new List<Student>()
+            {
+                new Student()
+                {
+                    Name = "Sam",
+                    ID = 101,
+                    Point = 57,
+                    Ability = new string[] { "JAVA", "Python", "C#" }
+                },
+
+                new Student()
+                {
+                    Name = "Edward",
+                    ID = 102,
+                    Point = 92,
+                    Ability = new string[] { "Alchemy", "CQC", "Math"}
+                },
+
+                new Student()
+                {
+                    Name = "Wendy",
+                    ID = 103,
+                    Point = 76,
+                    Ability = new string[] { "Mechanic", "Boxing", "Drawing"}
+                },
+
+                new Student()
+                {
+                    Name = "Asato",
+                    ID = 104,
+                    Point = 65,
+                    Ability = new string[] { "Dreaming", "Drawing", "Writing" }
+                }
+            };
+
+            return stus;
+        }
+    }
+}
